50e5b259542f62e8190837cf37bbe1dec8f6866e
Edward <edwardharks@yahoo.co.uk>
2016-08-06 23:25:47 +0100

    Added onViewAttached and detached. Also updated the demo app

---
+++ demo/src/main/java/com/edwardharker/demo/FooterViewType.java
@@ -0,0 +1,7 @@
+package com.edwardharker.demo;
+
+/**
+ * Created by edwardharker on 06/08/2016.
+ */
+public class FooterViewType {
+}
---
+++ library/src/main/java/com/edwardharker/multiitemadapter/SimpleMultiTypeViewHolder.java
@@ -0,0 +1,7 @@
+package com.edwardharker.multiitemadapter;
+
+/**
+ * Created by edwardharker on 06/08/2016.
+ */
+public class SimpleMultiTypeViewHolder {
+}
